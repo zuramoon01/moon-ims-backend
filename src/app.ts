5727d82c8eb44b2de3bc21b9d8a9f838bb4f9ad9
Zura Moon <zuramoon01@gmail.com>
2024-03-05 13:23:13 +0700

    feat(app): created app

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { apiRouter } from "./api";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors({ credentials: true }));
+app.use(express.json());
+
+app.use("/api", apiRouter);
+
+app.listen(PORT, () => {
+	console.log(`Server running on port http://127.0.0.1:${PORT}`);
+});
